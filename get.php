15239ca53389c8d124f8ec7a34890a2d0a3fa317
Dmitry Zhuravlev <dpzhuravlev@corp.finam.ru>
2023-08-25 16:20:50 +0300

    fix query

--- get.php
+++ get.php
@@ -5,25 +5,36 @@ require "pdo.php";
 header('Content-Type: application/json');
 $result = [];
 
-$team = $_GET['team'];
-if (empty($team)) {
+try {
+    $team = $_GET['team'];
+    if (empty($team)) {
+        throw new  Exception('Not found team in query');
+    }
+
+    $conn = getConnection();
+
+    $sql = "select * from main where team=:team";
+
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':team', $team);
+    $stmt->execute();
+
+    $data = $stmt->fetch(\PDO::FETCH_ASSOC);
+
+    if (empty($result)) {
+        throw new  Exception('Not found team in db');
+    }
+
+    $result = [
+        'status' => 'ok',
+        'data' => $data,
+    ];
+
+} catch (\Throwable $e) {
     $result = [
         'status' => 'fail',
-        'error' => 'Team is empty',
+        'error' => $e->getMessage(),
     ];
-    echo json_encode($result);
-    return;
 }
-
-$conn = getConnection();
-
-$sql = "select * from main where team=:team";
-
-$stmt = $conn->prepare($sql);
-$stmt->bindParam(':team', $team);
-$stmt->execute();
-
-$result = $stmt->fetch(\PDO::FETCH_ASSOC);
-
 echo json_encode($result);
 return;
