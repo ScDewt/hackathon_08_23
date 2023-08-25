1a65d0601aed5a03f4f513d826fa8eb98cef2b4a
Дмитрий Глижинский <daemon191@gmail.com>
2023-08-25 16:21:51 +0300

    fix webhook url

--- set.php
+++ set.php
@@ -6,7 +6,7 @@ require __DIR__ . '/vendor/autoload.php';
 
 $bot_api_key = '6363703996:AAF8bq4NQ8yd3PVO-7vFC1_LfvaNUmKRpTc';
 $bot_username = 'ga_hackathon_bot';
-$hook_url = 'http://92.63.177.178/webhook.php';
+$hook_url = 'https://hackathon0823.scdewt.ru/webhook.php';
 
 try {
     // Create Telegram API object
