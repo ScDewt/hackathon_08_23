efd5808e1fefe8eb491ecbb8b3e6252b2ee155ed
Dmitry Zhuravlev <dpzhuravlev@corp.finam.ru>
2023-08-25 16:06:08 +0300

    add filter team to get

--- get.php
+++ get.php
@@ -2,12 +2,17 @@
 
 require "pdo.php";
 
+$team = $_GET['team'];
+if (empty($team)) {
+    echo '{error: "team empty"}';
+}
+
 $conn = getConnection();
 
-$sql = "select * from main";
+$sql = "select * from main where team=?";
 
 $stmt = $conn->prepare($sql);
-//$stmt->bindParam(':status', $status);
+$stmt->bindParam(':team', $team);
 //$stmt->bindParam(':name', $name);
 //$stmt->bindParam(':hash', $hash);
 //$stmt->bindParam(':image_url', $imageUrl);
