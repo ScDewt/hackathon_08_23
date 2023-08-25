4cb05c7cb90439f219bbc08029bc35f5509c03ae
Дмитрий Глижинский <daemon191@gmail.com>
2023-08-25 15:54:40 +0300

    add composer

---
+++ set.php
@@ -0,0 +1,23 @@
+<?php
+
+
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+$bot_api_key = '6363703996:AAF8bq4NQ8yd3PVO-7vFC1_LfvaNUmKRpTc';
+$bot_username = 'ga_hackathon_bot';
+$hook_url = 'http://92.63.177.178/webhook.php';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Set webhook
+    $result = $telegram->setWebhook($hook_url);
+    if ($result->isOk()) {
+        echo $result->getDescription();
+    }
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // log telegram errors
+     echo $e->getMessage();
+}
---
+++ webhook.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// Load composer
+require __DIR__ . '/vendor/autoload.php';
+
+$bot_api_key = 'your:bot_api_key';
+$bot_username = 'username_bot';
+
+try {
+    // Create Telegram API object
+    $telegram = new Longman\TelegramBot\Telegram($bot_api_key, $bot_username);
+
+    // Handle telegram webhook request
+    $telegram->handle();
+} catch (Longman\TelegramBot\Exception\TelegramException $e) {
+    // Silence is golden!
+    // log telegram errors
+     echo $e->getMessage();
+}
