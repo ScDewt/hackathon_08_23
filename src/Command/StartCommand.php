671fd37fb9b120eb25cbd0bd255b53098fae1960
Дмитрий Глижинский <daemon191@gmail.com>
2023-08-25 16:55:12 +0300

    fix classes

--- src/Command/StartCommand.php
+++ src/Command/StartCommand.php
@@ -6,6 +6,7 @@ use Longman\TelegramBot\Commands\UserCommand;
 use Longman\TelegramBot\Entities\ServerResponse;
 use Longman\TelegramBot\Exception\TelegramException;
 use Longman\TelegramBot\Request;
+use Scdewt\Hackathon0823\DB\Connection;
 
 class StartCommand extends UserCommand
 {
@@ -30,10 +31,20 @@ class StartCommand extends UserCommand
 
         $chat_id = $message->getChat()->getId();
         $user_id = $message->getFrom()->getId();
-        $text    = trim($message->getText(true));
+        $name = $message->getFrom()->getFirstName() . " " . $message->getFrom()->getLastName() ;
+
+        $conn = Connection::getConnection();
+
+
+        $sql = "INSERT INTO main (person_id, person_name) VALUES(:person_id, :person_name) ON DUPLICATE KEY UPDATE person_name = :person_name";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':person_id', $user_id);
+        $stmt->bindParam(':person_name', $name);
+        $stmt->execute();
 
         $data = ['chat_id' => $chat_id];
-        $data['text'] = "Приветствую! Чтобы задать имя, воспользуйся командой /name ";
+        $data['text'] = "Приветствую, " . $name . "!";
 
         return Request::sendMessage($data);
     }
